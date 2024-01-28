b08f4e97779d3243d55dd972d4efc4650a3ae0ca
pranay <pranayharishchandra@gmail.com>
2024-01-28 16:02:28 +0530

    experiment

--- App.js
+++ App.js
@@ -2,4 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
-// const heading = React.createElement()
+ // const heading = React.createElement()
+
+ const heading = () => (
+  <h1 id='title' key='h2'>
+    Hello React
+  </h1>
+ )
+
+ const HeaderComponent = () => {
+  return (
+    <div>
+      {heading()}
+      <h1>
+        Hello World HeaderComponent
+      </h1>
+    </div>
+  )
+ }
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<HeaderComponent/>)
+
